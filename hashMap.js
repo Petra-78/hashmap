a8b9d55de8c1ed78c1a72d08959d60141a188db1
Petra-78 <partos.petra.0305@gmail.com>
2025-08-12 13:24:08 +0200

    initial commit

---
+++ hashMap.js
@@ -0,0 +1,19 @@
+class HashMap {
+  constructor(initialCapacity = 16, loadFactor = 0.75) {
+    this.capacity = initialCapacity;
+    this.loadFactor = loadFactor;
+    this.buckets = new Array(this.capacity).fill(null).map(() => []);
+    this.size = 0;
+  }
+
+  hash(key) {
+    let hashCode = 0;
+
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = primeNumber * hashCode + key.charCodeAt(i);
+    }
+
+    return hashCode;
+  }
+}
